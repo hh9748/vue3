7a5afe137b76f5e0733a1f1d2238f7fade10c36c
hh9748 <1694368626@qq.com>
2025-09-24 14:35:47 +0800

    学习第一天

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!-- html -->
+    <Person/>
 </template>
 
-<style scoped></style>
+<script lang="ts">
+import Person from './components/Person.vue';
+// JS或者TS
+  export default {
+    name:'App', //组件名
+    components: {Person} //注册组件
+  }
+</script>
+
+<style scoped>
+/* 样式 */
+  
+</style>
